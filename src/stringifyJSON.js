763ba4f8c0a04d0e6ef1662328b09535e479fa13
Gloria Ma <gloria.ma@aya.yale.edu>
2015-10-07 20:43:31 -0700

    Completed case-by-case version of stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,27 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null) {
+    return "null";
+  } else if (obj === undefined || typeof obj === "function") {
+    return undefined;
+  } else if (typeof obj === "string") {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    return '[' + obj.map(function(item) {
+      return stringifyJSON(item);
+    }).filter(function(item) {
+      return item !== undefined;
+    }).join(',') + ']';
+  } else if (typeof obj === "object") {
+    return '{' + Object.keys(obj).map(function(key) {
+      return stringifyJSON(key) && stringifyJSON(obj[key]) ?
+             stringifyJSON(key) + ':' + stringifyJSON(obj[key]) :
+             undefined;
+    }).filter(function(item) {
+      return item !== undefined;
+    }).join(',') + '}';
+  } else {
+    return obj.toString();
+  }
 };
